7c74a61793e6d7cb00209325c04ec8236b8348a5
TheStankela <ognjenogistankovic2002@gmail.com>
2023-07-06 01:28:50 +0200

    Added form for adding a club and logic for it

--- src/app/clubs/club-details/club-details.component.ts
+++ src/app/clubs/club-details/club-details.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, Input } from '@angular/core';
+import { CreateAddress } from 'src/app/models/createAddress';
+import { CreateClub } from 'src/app/models/createClub';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-club-details',
@@ -7,4 +11,21 @@ import { Component } from '@angular/core';
 })
 export class ClubDetailsComponent {
 
+  @Input()
+  club = new CreateClub();
+  
+  baseUrl = environment.apiURL;
+  constructor(private httpClient: HttpClient){
+    this.club.address = new CreateAddress();
+  }
+  handleCreate(){
+    this.httpClient.post<any>(this.baseUrl + '/Club', this.club).subscribe({
+      next: res => {
+        console.log(res);
+      },
+      error: err => {
+        console.log(err);
+      }
+    })
+  }
 }
