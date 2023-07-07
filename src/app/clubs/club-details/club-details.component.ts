a6765e81d37f7a65fc7f6e6a21206d9689ae410f
TheStankela <ognjenogistankovic2002@gmail.com>
2023-07-07 19:44:08 +0200

    Added authorization

--- src/app/clubs/club-details/club-details.component.ts
+++ src/app/clubs/club-details/club-details.component.ts
@@ -1,7 +1,9 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CreateAddress } from 'src/app/models/createAddress';
 import { CreateClub } from 'src/app/models/createClub';
+import { AuthService } from 'src/app/services/auth.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -9,17 +11,26 @@ import { environment } from 'src/environments/environment';
   templateUrl: './club-details.component.html',
   styleUrls: ['./club-details.component.scss']
 })
-export class ClubDetailsComponent {
+export class ClubDetailsComponent implements OnInit{
 
   @Input()
   club = new CreateClub();
   
   baseUrl = environment.apiURL;
-  constructor(private httpClient: HttpClient){
+
+  constructor(private httpClient: HttpClient, private authService: AuthService, private router: Router){
     this.club.address = new CreateAddress();
   }
+
+  ngOnInit(): void {
+    if(!this.authService.loggedIn()){
+      alert("Login to create new club.");
+      this.router.navigate(['auth/login']);
+    }
+  }
+
   handleCreate(){
-    this.httpClient.post<any>(this.baseUrl + '/Club', this.club).subscribe({
+    this.httpClient.post<any>(this.baseUrl + '/Club', this.club, {withCredentials: true}).subscribe({
       next: res => {
         console.log(res);
       },
