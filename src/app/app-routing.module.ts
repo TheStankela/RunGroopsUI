ff25c9936bab0986d074146192127fea245c40b5
TheStankela <ognjenogistankovic2002@gmail.com>
2023-07-19 01:29:40 +0200

    Modules and routing updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,18 +6,39 @@ import { RegisterComponent } from './auth/register/register.component';
 import { ClubsComponent } from './clubs/clubs/clubs.component';
 import { CreateClubComponent } from './clubs/create-club/create-club.component';
 import ClubSingleComponent from './clubs/club-single/club-single.component';
-import { ProfileComponent } from './profile/profile.component';
+import { ProfileComponent } from './account/profile/profile.component';
 import { UpdateClubComponent } from './clubs/update-club/update-club.component';
+import { RacesComponent } from './races/races/races.component';
+import { RaceCreateComponent } from './races/race-create/race-create.component';
+import { RaceUpdateComponent } from './races/race-update/race-update.component';
+import { RaceSingleComponent } from './races/race-single/race-single.component';
+import { EditProfileComponent } from './account/edit-profile/edit-profile.component';
+import { DashboardComponent } from './account/dashboard/dashboard.component';
+import { UsersComponent } from './users/users/users.component';
+import { UserSingleComponent } from './users/user-single/user-single.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent},
+
   { path: 'auth/login', component: LoginComponent},
   { path: 'auth/register', component: RegisterComponent},
+
   { path: 'profile', component: ProfileComponent},
-  {path: 'clubs', component:ClubsComponent},
+  { path: 'profile/dashboard', component: DashboardComponent},
+  { path: 'profile/edit', component: EditProfileComponent},
+
+  {path: 'clubs', component:  ClubsComponent},
   {path:'clubs/create', component: CreateClubComponent},
   {path:'clubs/edit/:id', component: UpdateClubComponent},
   {path:'clubs/:id', component: ClubSingleComponent},
+
+  {path: 'races', component:  RacesComponent},
+  {path:'races/create', component: RaceCreateComponent},
+  {path:'races/edit/:id', component: RaceUpdateComponent},
+  {path:'races/:id', component: RaceSingleComponent},
+
+  { path: 'users', component: UsersComponent},
+  { path: 'users/:id', component: UserSingleComponent},
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,8 +24,20 @@ import {MatPaginatorModule} from '@angular/material/paginator';
 import { NgxUiLoaderModule, NgxUiLoaderHttpModule } from 'ngx-ui-loader';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ProfileComponent } from './profile/profile.component';
+import { ProfileComponent } from './account/profile/profile.component';
 import { UpdateClubComponent } from './clubs/update-club/update-club.component';
+import { RacesComponent } from './races/races/races.component';
+import { RaceSingleComponent } from './races/race-single/race-single.component';
+import { RaceCreateComponent } from './races/race-create/race-create.component';
+import { RaceUpdateComponent } from './races/race-update/race-update.component';
+import { AuthService } from './services/auth.service';
+import { ClubService } from './services/club.service';
+import { RaceService } from './services/race.service';
+import { AccountService } from './services/account.service';
+import { EditProfileComponent } from './account/edit-profile/edit-profile.component';
+import { DashboardComponent } from './account/dashboard/dashboard.component';
+import { UsersComponent } from './users/users/users.component';
+import { UserSingleComponent } from './users/user-single/user-single.component';
 
 @NgModule({
   declarations: [
@@ -41,6 +53,15 @@ import { UpdateClubComponent } from './clubs/update-club/update-club.component';
     CreateClubComponent,
     ProfileComponent,
     UpdateClubComponent,
+    RacesComponent,
+    RaceSingleComponent,
+    RaceCreateComponent,
+    RaceUpdateComponent,
+    EditProfileComponent,
+    DashboardComponent,
+    UsersComponent,
+    UserSingleComponent,
+    UsersComponent,
   ],
   imports: [
     BrowserModule,
@@ -69,7 +90,7 @@ import { UpdateClubComponent } from './clubs/update-club/update-club.component';
     progressBar: true
   })
   ],
-  providers: [],
+  providers: [AuthService, ClubService, RaceService, AccountService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
